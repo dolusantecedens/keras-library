5613a63276e96a86f289c71a0b8767a759835f69
dolusantecedens <aleksander.marciniak@gmail.com>
2022-12-07 13:50:05 +0100

    first add

---
+++ main.py
@@ -0,0 +1,6 @@
+from keras import layers
+from keras import models 
+
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3,3), activation='relu', input_shape=(28, 28 ,1 )))
+model.add(layers.MaxPooling2D, (2,2))
